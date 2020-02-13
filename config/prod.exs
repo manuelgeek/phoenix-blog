976c46ad098a2ac9924d6b68bf4749fe25a15dea
manuelgeek <emashmagak@appslab.co.ke>
2020-02-13 14:09:04 +0300

    herok deploy partial

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,10 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :blog, BlogWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+       url: [scheme: "https", host: "phx-blog.herokuapp.com", port: 443],
+       force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -52,4 +54,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-import_config "prod.secret.exs"
+#import_config "prod.secret.exs"
