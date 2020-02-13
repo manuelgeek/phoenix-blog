2381b2ad656cae5f0e8a0a78178e1b5187c9414a
manuelgeek <emashmagak@appslab.co.ke>
2020-02-13 20:38:55 +0300

    added heroku url;

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :blog, BlogWeb.Endpoint,
-       url: [scheme: "https", host: "phx-blog.herokuapp.com", port: 443],
+       url: [scheme: "https", host: "phx-blog-manu.herokuapp.com", port: 443],
        force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
