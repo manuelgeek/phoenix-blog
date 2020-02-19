432023ff8427997a9bbae3d3ab62d8045829c879
manuelgeek <emashmagak@appslab.co.ke>
2020-02-19 13:24:35 +0300

    updated v

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Blog.MixProject do
     [
       app: :blog,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
