6f4ab1cec5388d3f8f3753f12998d590ecf6bff5
manuelgeek <emashmagak@appslab.co.ke>
2020-02-19 13:52:58 +0300

    addeddb url

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :blog, BlogWeb.Endpoint,
-       url: [scheme: "https", host: "phx-blog-manu.herokuapp.com", port: 443],
-       force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "https", host: "phx-blog-manu.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
@@ -54,11 +54,11 @@ config :logger, level: :info
 
 # Configure your database
 config :hello, Hello.Repo,
-       adapter: Ecto.Adapters.Postgres,
-       pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-       ssl: true,
-       url: System.get_env("DATABASE_URL")
-       
+  adapter: Ecto.Adapters.Postgres,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true,
+  url: System.get_env("HEROKU_POSTGRESQL_SILVER_URL")
+
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-#import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
