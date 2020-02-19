a8eb4b54627b02ef0a796b276f69a789738329a3
manuelgeek <emashmagak@appslab.co.ke>
2020-02-19 15:10:26 +0300

    latest

--- config/prod.exs
+++ config/prod.exs
@@ -58,7 +58,7 @@ config :hello, Hello.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
+  ssl: true,ga
   show_sensitive_data_on_connection_error: true
 
 # Finally import the config/prod.secret.exs which loads secrets
