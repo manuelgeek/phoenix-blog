bcf0da3387b5187723329f0925a43236f4f96b09
manuelgeek <emashmagak@appslab.co.ke>
2020-02-19 16:32:12 +0300

    fixed procfile

--- config/prod.exs
+++ config/prod.exs
@@ -11,6 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :blog, BlogWeb.Endpoint,
   http: [port: {:system, "PORT"}],
+  load_from_system_env: true,
   url: [scheme: "https", host: "phx-blog-manu.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
