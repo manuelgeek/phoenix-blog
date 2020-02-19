44edcefd1d32de0086eef65accc7226fbf0d713c
manuelgeek <emashmagak@appslab.co.ke>
2020-02-19 15:16:18 +0300

    changes db ;

--- config/prod.exs
+++ config/prod.exs
@@ -54,11 +54,11 @@ config :logger, level: :info
 # Check `Plug.SSL` foheroku pg:diagnose --app sushir all available options in `force_ssl`.
 
 # Configure your database
-config :hello, Hello.Repo,
+config :blog, Blog.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true,ga
+  ssl: true,
   show_sensitive_data_on_connection_error: true
 
 # Finally import the config/prod.secret.exs which loads secrets
